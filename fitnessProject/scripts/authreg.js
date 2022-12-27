969e0cfb401f3bcd57ca74c03e9c737a0e9f57fb
Kirill <104451159+Modestra@users.noreply.github.com>
2022-12-27 12:54:50 +0700

    Создание верстки сайта авторизации и регистрации

---
+++ fitnessProject/scripts/authreg.js
@@ -0,0 +1,12 @@
+function enterInSite(){
+    window.open("D:/Fitness/fitnessProject/main.html")
+    this.close()
+}
+function registration(){
+    window.open("D:/Fitness/fitnessProject/registration.html")
+    this.close()
+}
+function authorisation(){
+    window.open("D:/Fitness/fitnessProject/authorization.html")
+    this.close()
+}
