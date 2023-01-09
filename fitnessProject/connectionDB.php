96ee4adfb33c496f0a1354d0cc4acc6bae12edd5
Kirill <104451159+Modestra@users.noreply.github.com>
2023-01-10 00:13:11 +0700

    Подключение базы данных к сайту

--- fitnessProject/connectionDB.php
+++ fitnessProject/connectionDB.php
@@ -1,4 +1,13 @@
 <?php
-    $login = $_POST['login']
-    echo $login;
+    $login = $_GET['login'];
+    $email = $_GET['e-mail'];
+    $pass = $_GET['password'];
+    $connect = mysqli_connect('192.168.56.1', 'root', '', 'cakesiteDB');
+    if($connect == false){
+        echo "Не работает";
+    }
+    else{
+        $connect->query("INSERT INTO auth(email, login, password) VALUES ('$email', '$login', '$pass')");
+    }
+
 ?>
--- fitnessProject/scripts/authreg.js
+++ fitnessProject/scripts/authreg.js
@@ -1,18 +1,9 @@
-$(document).ready(()=>{
-    $(".auth").load("../fitnessProject/connectionDB.php")
-});
-$('#enter').on('clink', ()=>{
-    alert("Хуяк");
-});
-function enterInSite(){
-    window.open("D:/Fitness/fitnessProject/main.html")
-    this.close()
-}
+let input = document.querySelector('#data');
 function registration(){
-    window.open("D:/Fitness/fitnessProject/registration.html")
+    window.open("../fitnessProject/registration.html")
     this.close()
 }
 function authorisation(){
-    window.open("D:/Fitness/fitnessProject/authorization.html")
+    window.open("../fitnessProject/authorization.html")
     this.close()
 }
