05c68f66e4d7d5225a552fd5fb62f2a20265d26b
Kirill <104451159+Modestra@users.noreply.github.com>
2023-11-22 22:07:17 +0700

    Создание reset-styles

--- fitness/src/app/app-routing.module.ts
+++ fitness/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {WorkoutComponent} from "./workout/workout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: AppComponent
+  },
+  {
+    path: "workout",
+    component: WorkoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
